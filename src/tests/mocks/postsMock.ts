a5255fac44edf0c5b2739fc9f0b6b3e8ee7ac0e0
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-27 11:50:19 -0300

    feat: postsMock

---
+++ src/tests/mocks/postsMock.ts
@@ -0,0 +1,51 @@
+export const responseMock = [{
+  "history": [
+    {
+      "title": "test",
+      "body": "testing",
+      "updatedAt": "2022-08-26T18:41:10.296Z",
+      "_id": "63091a8ef1597685674fd06c"
+    }
+  ],
+  "_id": "63091a8ef1597685674fd06b",
+  "createdAt": "2022-08-26T19:10:06.620Z",
+  "updatedAt": "2022-08-26T19:10:06.620Z"
+}];
+
+export const createPostMock = {
+  history: [{
+    title: 'test',
+    body: 'testing',
+    updatedAt: new Date(),
+  }],
+}
+
+export const updateResponseMock = {
+  "_id": "63091a8ef1597685674fd06b",
+  "history": [
+    {
+      "title": "test2",
+      "body": "testing2",
+      "updatedAt": "2022-08-27T18:53:43.234Z",
+      "_id": "63093e693cf4365539d649fe"
+    },
+    {
+      "title": "test",
+      "body": "testing",
+      "updatedAt": "2022-08-26T18:41:10.296Z",
+      "_id": "63091a8ef1597685674fd06c"
+    }
+  ],
+  "createdAt": "2022-08-26T19:10:06.620Z",
+  "updatedAt": "2022-08-26T21:43:05.074Z"
+}
+
+export const updatePostMock = {
+  history: [{
+    title: "test2",
+    body: "testing2",
+    updatedAt: new Date(),
+  }],
+};
+
+export const MOCK_ID = '63091a8ef1597685674fd06b';
