062ddfa3beb3eeec1fe6f88e2f4b43f545fdcbe5
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-27 15:55:23 -0300

    feat: Controller test > readOne method

--- src/tests/controller/controller.test.ts
+++ src/tests/controller/controller.test.ts
@@ -3,7 +3,7 @@ import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../../server';
 import PostModel from '../../model/PostModel';
-import { createRequestBody, invalidCreatePostMock, responseBodyMock, responseMock, updateResponseMock } from '../mocks/postsMock';
+import { createRequestBody, invalidCreatePostMock, MOCK_ID, responseBodyMock, responseMock } from '../mocks/postsMock';
 
 chai.use(chaiHttp);
 
@@ -68,6 +68,44 @@ describe('PostController', () => {
 
       expect(chaiHttpResponse).to.have.status(200);
       expect(chaiHttpResponse.body).to.be.deep.eq(responseBodyMock);
+    });
+  });
+
+  describe('readOne', () => {
+    before(() => Sinon
+      .stub(postModel.model, 'findById')
+      .onFirstCall().resolves(responseMock[0])
+      .onSecondCall().throws()
+      .onThirdCall().throws());
+
+    it('should return status 200 and a post when the id is valid', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get(`/${MOCK_ID}`)
+        .send();
+
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.be.deep.eq(responseBodyMock[0]);
+    });
+
+    it('should return status 400 and an invalid id error message when the id is invalid', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/invalidId')
+        .send();
+
+      expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body.error).to.be.eq('Id must have 24 hexadecimal characters');
+    });
+
+    it('should return status 500 and an "Internal Server Error" message', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get(`/${MOCK_ID}`)
+        .send();
+      
+      expect(chaiHttpResponse).to.have.status(500);
+      expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
     });
   });
 });
