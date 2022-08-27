59b6e8d4cb50ce4ac2e72beb59cdbc0d4aa48bd6
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-27 15:25:02 -0300

    feat: Controller test > read method

--- src/tests/controller/controller.test.ts
+++ src/tests/controller/controller.test.ts
@@ -12,6 +12,8 @@ const app = server.getApp();
 describe('PostController', () => {
   const postModel = new PostModel();
 
+  let chaiHttpResponse;
+
   afterEach(() => Sinon.restore());
 
   describe('create', () => {
@@ -22,7 +24,7 @@ describe('PostController', () => {
     .onThirdCall().throws());
 
     it('should return a status 201 and a new post created when data is valid', async () => {
-      const chaiHttpResponse = await chai
+      chaiHttpResponse = await chai
         .request(app)
         .post('/')
         .send(createRequestBody);
@@ -33,7 +35,7 @@ describe('PostController', () => {
     });
 
     it('should return status 400 when data is invalid', async () => {
-      const chaiHttpResponse = await chai
+      chaiHttpResponse = await chai
         .request(app)
         .post('/')
         .send(invalidCreatePostMock);
@@ -43,13 +45,29 @@ describe('PostController', () => {
     });
 
     it('should return status 500 and an "Internal Server Error" message', async () => {
-      const chaiHttpResponse = await chai
+      chaiHttpResponse = await chai
       .request(app)
       .post('/')
       .send(createRequestBody);
 
       expect(chaiHttpResponse).to.have.status(500);
       expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
+    });
+  });
+
+  describe('read', () => {
+    before(() => Sinon
+      .stub(postModel.model, 'find')
+      .resolves(responseMock));
+
+    it('should return status 200 and a list of all tasks', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/')
+        .send();
+
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.be.deep.eq(responseBodyMock);
     });
   });
 });
