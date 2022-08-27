1dc12d41299445c95329010bb5e3a8e5d1225a68
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-27 11:49:48 -0300

    feat: Router

---
+++ src/routes/Router.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import Controller from '../controller';
+
+export default class CustomRouter<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRoute(
+    controller: Controller<T>,
+    route: string = controller.route,
+  ) {
+    this.router.route(route)
+      .post(controller.create)
+      .get(controller.read);
+    this.router.route(`${route}:id`)
+      .get(controller.readOne)
+      .put(controller.update)
+      .delete(controller.delete);
+  }
+}
